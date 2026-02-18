c9864dc19c8b1fc807a42ca5f3ad7641065a71de
Alamin-Rintu <alaminrintu01@gmail.com>
2026-02-18 16:31:23 +0600

    added history section

--- script/addmoney.js
+++ script/addmoney.js
@@ -19,7 +19,7 @@ document.getElementById("add-money-btn").addEventListener("click", function () {
   // 4.check the current balace
   const balanceElement = document.getElementById("balance");
   const balance = balanceElement.innerText;
-//   console.log(balance);
+  //   console.log(balance);
 
   const newBalance = Number(addMoneyAmount) + Number(balance);
   //   console.log( 'New Balance',newBalance)
@@ -30,6 +30,23 @@ document.getElementById("add-money-btn").addEventListener("click", function () {
          ${bankAccount}
           at ${new Date()}`);
     balanceElement.innerText = newBalance;
+    // 1. history-container ke shore niye asbo
+    const history = document.getElementById("history-container");
+
+    // 2. new div create korbo
+    const newHistory = document.createElement("div");
+    // 3. new div innerHTML add korbo
+    newHistory.innerHTML = `
+     <div class="history-card p-5 bg-base-100 rounded-lg shadow space-y-5">
+    <p>
+      Add Money Successful from 
+      <span class="font-semibold">${bankAccount}</span> 
+      at ${new Date().toLocaleString()}
+    </p>
+  </div>
+    `;
+    // 4. history contianer e new div append korbo
+    history.append(newHistory);
   } else {
     alert("Invalid Pin");
     return;
